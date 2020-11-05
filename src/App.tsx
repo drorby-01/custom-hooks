4f1186a343e767ec1ddd8386171324ba062323ea
Gal Amouyal <gal.amouyal@ibm.com>
2020-11-05 21:54:05 +0200

    src

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./components/login";
+
+// import "./App.css";
+
+import Register from "./components/register";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/register.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import useCustomForm from "../hooks/useCustomForm";
+
+export default function Register() {
+  const initialState = { firstName: "", lastName: "", password: "" };
+
+  //with CustomHook - useCustomForm
+  const [userData, useChangeB] = useCustomForm(initialState);
+
+  // without Custom Hook
+  // const [userData, setUserData] = useState(initialState);
+  // const onChangeInput = (e: any) => {
+  //   const { name, value } = e.target;
+  //   setUserData({ ...userData, [name]: value });
+  // };
+
+  const register = () => {
+    console.log(userData);
+  };
+
+  return (
+    <div>
+      <div>
+        <div className="container">
+          <form>
+            <div className="row">
+              <div className="col-25">
+                <label>First Name</label>
+              </div>
+              <div className="col-75">
+                <input
+                  onChange={useChangeB}
+                  type="text"
+                  id="fname"
+                  name="firstName"
+                  placeholder="Your name.."
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label>Last Name</label>
+              </div>
+              <div className="col-75">
+                <input
+                  onChange={useChangeB}
+                  type="text"
+                  id="lname"
+                  name="lastName"
+                  placeholder="Your last name.."
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label>Password</label>
+              </div>
+              <div className="col-75">
+                <input
+                  onChange={useChangeB}
+                  type="text"
+                  id="password"
+                  name="password"
+                  placeholder="Your password.."
+                />
+              </div>
+            </div>
+            {/* <div className="row">
+              <div className="col-25">
+                <label>Country</label>
+              </div>
+              <div className="col-75">
+                <select id="country" name="country">
+                  <option value="australia">Australia</option>
+                  <option value="canada">Canada</option>
+                  <option value="usa">USA</option>
+                </select>
+              </div>
+            </div> */}
+
+            <div className="row">
+              <input type="button" onClick={register} value="Send" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
